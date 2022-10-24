26281a0d936fe2bf68474255ef6e7cb7d322a764
Sharipov Timur <deakoks@gmail.com>
2022-10-24 11:08:06 +0300

    Тема Функции

---
+++ JS-test/main-js.js
@@ -0,0 +1,10 @@
+function sayHi(name) {
+    console.log(`Привет, ${name}! Как твои дела?`);
+}
+sayHi('Тимур');
+
+
+const arrSayHi = (name) => {
+    console.log(`Привет, ${name}! Как твои дела?`);  
+}
+arrSayHi('Марат');
