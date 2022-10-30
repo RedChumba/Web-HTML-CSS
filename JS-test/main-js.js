15223a60d5b2834c2c1f5e7448e4ffb577f364a5
Sharipov Timur <deakoks@gmail.com>
2022-10-30 11:16:21 +0300

    Филтры

--- JS-test/main-js.js
+++ JS-test/main-js.js
@@ -1,19 +1,2 @@
-//function sayHi(name) {
- //   console.log(`Привет, ${name}! Как твои дела?`);
-//}
-//sayHi('Тимур');
 
 
-//const arrSayHi = (name) => {
-//    console.log(`Привет, ${name}! Как твои дела?`);  
-//}
-//arrSayHi('Марат');
-
-
-const autoBrand1 = 'Audi';
-const autoBrand2 = 'BMW';
-const autoBrand3 = 'Mazda';
-const autoBrand4 = 'Toyota';
-
-const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
-console.log(autoBrands);
